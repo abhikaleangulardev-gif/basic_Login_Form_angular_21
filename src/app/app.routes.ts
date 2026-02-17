65376675cf4e3bedc8bc69e9df78fa92a876cc12
abhishek.kale <abhisahebpatil@gmail.com>
2026-02-17 11:50:55 +0530

    develop-register-form-is-completed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'register-page', pathMatch: 'full' },
+    { path: 'login-page', loadComponent: () => import('./component/login-pages/login-pages').then((c) => c.LoginPages) },
+    { path: 'register-page', loadComponent: () => import('./component/register-pages/register-pages').then((c) => c.RegisterPages) },
+    { path: 'dashboard-page', loadComponent: () => import('./component/dashboard-page/dashboard-page').then((c) => c.DashboardPage) },
+];
