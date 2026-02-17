e85540c2895f03caadfa727253111d14c867bb15
abhishek.kale <abhisahebpatil@gmail.com>
2026-02-17 11:09:49 +0530

    setup-login-form-project

---
+++ src/app/component/register-pages/register-pages.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-pages',
+  imports: [],
+  templateUrl: './register-pages.html',
+  styleUrl: './register-pages.css',
+})
+export class RegisterPages {
+
+}
